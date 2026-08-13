24e490e007fc923fc0bed0365c16ccd530cebcc2
darknhckr <darknhckr@example.com>
2026-08-13 09:55:10 +0000

    Initial commit from NAVEEDSNDSBOT.zip

---
+++ SY/ddos.js
@@ -0,0 +1,25 @@
+/*
+ * © 2026 NAVEED SND (VOIDSEC)
+ *
+ * ⚠️ COPYRIGHT NOTICE
+ * This source code is protected under copyright law.
+ * Any form of re-uploading, recoding, modification,
+ * selling, or redistribution WITHOUT explicit permission
+ * from the original author is strictly prohibited.
+ *
+ * ❌ NO CREDIT = NO PERMISSION
+ * ❌ DO NOT CLAIM THIS CODE AS YOUR OWN
+ *
+ * ✔️ Usage or modification is allowed ONLY
+ * with prior permission and proper credit.
+ *
+ * OFFICIAL LINKS (ONLY):
+ * YouTube   : https://youtube.com/
+ * Instagram : sabi
+ * Telegram  : https://t.me/dark_n_hacker
+ * GitHub    : https://github.com/darknhckr
+ * WhatsApp  : +923174886361
+ *
+ * Violations may result in DMCA takedown
+ * or termination of the Telegram bot.
+ */
---
+++ config.js
@@ -0,0 +1,50 @@
+/*
+ * © 2026 NAVEED SND (VOIDSEC)
+ *
+ * ⚠️ COPYRIGHT NOTICE
+ * This source code is protected under copyright law.
+ * Any form of re-uploading, recoding, modification,
+ * selling, or redistribution WITHOUT explicit permission
+ * from the original author is strictly prohibited.
+ *
+ * ❌ NO CREDIT = NO PERMISSION
+ * ❌ DO NOT CLAIM THIS CODE AS YOUR OWN
+ *
+ * ✔️ Usage or modification is allowed ONLY
+ * with prior permission and proper credit.
+ *
+ * OFFICIAL LINKS (ONLY):
+ * YouTube   : https://youtube.com/
+ * Instagram : sabi
+ * Telegram  : https://t.me/dark_n_hacker
+ * GitHub    : https://github.com/darknhckr
+ * WhatsApp  : +923174886361
+ *
+ * Violations may result in DMCA takedown
+ * or termination of the Telegram bot.
+ */
+module.exports = {
+    mainToken: '8855128989:AAGcFSIeQZ8KnfkTRp9asgn2aXSlDTOFrBg', // Bot Token Here 
+    
+    S7: '👑↡⏤͟͟͞🦋⃝⃪ ɳαʋҽҽԃ кнαη', // Owner, Name
+    
+    adminId: '7817582859', // Owner Chat ID ( control /address, /delresell )
+    
+    channel: 'https://t.me/darkdhacker', // TG channel link
+    
+    group: 'https://t.me/darkdhacke', // TG group link
+    
+    youtube: 'https://youtube.com/', // YouTube Link
+    
+    waChannel: 'https://whatsapp.com/channel/0029Vb91sBWBadmV7fE4TL1C', // WhatsApp Channel Link
+    
+    instagram: 'https://www.instagram.com/darkdhacker?igsh=eWg2d2p6YjJpZ3Zh', // Instagram Link 
+    
+    channelId: '-1003963016482', // channel ID bot must be admin in the channel
+    
+    groupId: '-1004418227150', // group ID bot must be admin in the group
+    
+    bot: '𝙉𝘼𝙑𝙀𝙀𝘿 𝙎𝙉𝘿 𝘽𝙐𝙂 𝘽𝙊𝙏 V1', // Bot name 
+    
+    logo: './SY/Loves.jpg' // Bot logo path or URL
+};
